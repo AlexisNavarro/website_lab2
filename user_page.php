4fe5ceddb0ab35ff029ad91a2ab3c381b49ce836
Alexis Navarro <navarroalexis007@gmail.com>
2022-11-13 19:30:18 -0700

    let the user change its password now

--- user_page.php
+++ user_page.php
@@ -7,7 +7,35 @@
 
     <body>
     <h1> user page</h1>    
-</body>
 
+    <?php
+    session_start();
+    require_once "config.php";
+    
+        try {
+            $pdo = new PDO($atrr, $username, $password, $opts);
+        }catch (PDOException $e){
+            throw new PDOException($e->getMessage(), (int)$e->getCode());
+        }
+        
+        $curr_user = $_SESSION['user'];
+        $query = "SELECT * FROM users WHERE username='" . $curr_user ."';";
+        $result = $pdo->query($query);
+        while($row = $result->fetch()){
+            $username = htmlspecialchars($row['username']);
+            $firstname = htmlspecialchars($row['first_name']);
+            $lastname = htmlspecialchars($row['last_name']);
+            $last_log_in = htmlspecialchars($row['last_log_in']);
+            echo $username." ".$firstname." ".$lastname." ".$last_log_in."<br>";
+            
+        }
+
+        
+    ?>
+</body>
+<div class = 'form-group'>
+                <a href='new_password.php'>change password</a><br><br> 
+                
+</div>
 </head>
 </html>
